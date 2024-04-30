a2ead7c78f6d3a6d92d2e3baa220e9b19a16a295
Oscar Bäck <oscar.back96@outlook.com>
2024-04-30 16:14:43 +0200

    Add Item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,29 @@ export class AppComponent {
   componentTitle = "My To Do List";
 
   filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: false },
+    { description: "sleep", done: false },
+    { description: "code", done: false },
+    { description: "repeat", done: false },
+  ]
+
+  get items() {
+    if(this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => 
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description : string){
+    if(!description) return;
+
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
 }
