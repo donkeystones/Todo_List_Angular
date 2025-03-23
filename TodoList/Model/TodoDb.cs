51243787e6286479ce76a9289deee01be5f73866
donkeystones <oscar.back96@outlook.com>
2025-03-23 10:20:26 +0100

    fixed solution issue, again

---
+++ TodoList/Model/TodoDb.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.DTO;
+
+namespace TodoList.Model;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options) {}
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
